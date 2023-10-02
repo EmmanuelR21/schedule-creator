3a4ddf96cdae7d3bee75f948411f2dd254dbd3cd
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2023-10-02 10:37:18 -0400

    added Sunday creation function to add to Schedule Grid

--- src/ScheduleGrid.jsx
+++ src/ScheduleGrid.jsx
@@ -1,15 +1,17 @@
 import { useContext } from "react";
 import Context from "./Context/Context";
+import createSundaysList from "./Functions/createSundaysList";
 
 export default function ScheduleGrid() {
   const { weeks } = useContext(Context)
+  const Sundays = createSundaysList(weeks.current.value)
 
   return (
     <div className="grid grid-cols-4 m-6">
       {
-        Array(+weeks.current.value).fill(0).map(x => 
+        Sundays.map((date) =>
             <>
-              <div className="border-r-0 border-l border-y border-black p-2"></div>
+              <div className="border-r-0 border-l border-y border-black p-2">{ date }</div>
               <div className="col-span-3 border-x border-y border-black p-2"></div>
             </>
         )
