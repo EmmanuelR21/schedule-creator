b9425206f1a83d996670ed9beb39fee6cb4d6ce9
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2023-10-02 11:22:02 -0400

    removed unsused useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import ScheduleGrid from "./ScheduleGrid";
 import CreateDaysButton from "./CreateDaysButton";
 import Context from "./Context/Context";
