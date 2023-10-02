e3d9aa6b51d669a95a4b328d18ea02ef8e38ffa1
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2023-10-02 10:35:34 -0400

    created function to produce Sunday dates

---
+++ src/Functions/Maps/numToMonth.js
@@ -0,0 +1,5 @@
+const NUM_TO_MONTH = [
+    "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+]
+
+export default NUM_TO_MONTH
---
+++ src/Functions/createSundaysList.js
@@ -0,0 +1,24 @@
+import NUM_TO_MONTH from "./Maps/numToMonth"
+
+const createSundaysList = (numOfSundays) => {
+    console.log(numOfSundays)
+    const CURRENT_DATE = new Date()
+    const SUNDAYS = []
+
+    while (CURRENT_DATE.getDay() !== 0) {
+        CURRENT_DATE.setDate(CURRENT_DATE.getDate() + 1)
+    }
+
+    for (let i = 0; i < numOfSundays; i++) {
+        SUNDAYS.push(`${NUM_TO_MONTH[CURRENT_DATE.getMonth()]} ${CURRENT_DATE.getDate()}, ${CURRENT_DATE.getFullYear()}`)
+
+        if (CURRENT_DATE.getDate() + 7 < CURRENT_DATE.getDate()) {
+            CURRENT_DATE.setMonth(CURRENT_DATE.getMonth() + 1)
+        } else {
+            CURRENT_DATE.setDate(CURRENT_DATE.getDate() + 7)
+        }
+    }
+    return SUNDAYS
+}
+
+export default createSundaysList;
