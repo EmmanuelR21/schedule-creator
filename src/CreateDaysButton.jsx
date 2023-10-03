eb52c20c65dd9c64a6e78037199406427639d94b
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2023-10-03 16:06:56 -0400

    removed unnecessary function

--- src/CreateDaysButton.jsx
+++ src/CreateDaysButton.jsx
@@ -4,17 +4,11 @@ import Context from "./Context/Context";
 export default function CreateDaysButton() {
     const { weeks, changeStateOfButtonClicked } = useContext(Context)
 
-    function createSchedule(e, numOfRows) {
-        e.preventDefault();
-        console.log(numOfRows)
-        changeStateOfButtonClicked(true)
-    }
-
     return (
         <label className="flex flex-col items-center">
             How many weeks would you like to create?
             <input className="border-x border-y border-black rounded" type="number" ref={weeks} />
-            <button className="bg-blue-950 text-white rounded py-1 px-2 mt-4" onClick={(e) => createSchedule(e, weeks.current.value)}>Create Schedule</button>
+            <button className="bg-blue-950 text-white rounded py-1 px-2 mt-4" onClick={() => changeStateOfButtonClicked(true)}>Create Schedule</button>
         </label>
     )
 }
