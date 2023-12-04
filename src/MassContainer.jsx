0316f26f1c2e4774015000922e16dedc14f5d0e5
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2023-12-04 11:34:05 -0500

    abstracted ServerInputContainer to reduce repetition, added english and spanish mass servers list

--- src/MassContainer.jsx
+++ src/MassContainer.jsx
@@ -1,45 +1,20 @@
 import { useState } from "react"
-import ServerContainer from "./ServerContainer"
+import ServerInputContainer from "./ServerInputContainer"
 
-export default function MassContainer() {
-    const [servers, setServers] = useState([])
-    const [isBilingual, setMassType] = useState(false)
-
-    const handleDrag = (e) => {
-        e.preventDefault()
-    }
-
-    const handleDrop = (e) => {
-        const data = e.dataTransfer.getData('alter-server')
-        setServers([...servers, data])
-    }
-
-    const handleClick = (e) => {
-        e.preventDefault()
-        const newArr = [...servers]
-        newArr.splice(e.target.accessKey, 1)
-        setServers(newArr)
-    }
+export default function MassContainer({ isBilingual }) {
+    const [bilingualServers, setBilingualServers] = useState([])
+    const [spanishServers, setSpanishServers] = useState([])
+    const [englishServers, setEnglishServers] = useState([])
 
     return (
-        <div className="flex col-span-3 border-x border-y border-black p-4">
+        <div className="flex col-span-3 border-x border-y border-black p-2">
             {
                 !isBilingual ?
-                    <div className="flex border border-black px-2 w-auto rounded-md" onDragOver={handleDrag} onDrop={handleDrop}>
-                        {
-                            servers.map((server, i) => {
-                                return (
-                                    <ServerContainer name={server} index={i} clickFunction={handleClick} />
-                                )
-                            })
-                        }
-                    </div>
+                    <ServerInputContainer servers={bilingualServers} setServers={setBilingualServers} />
                     :
                     <>
-                        <div>
-                        </div>
-                        <div>
-                        </div>
+                        <ServerInputContainer servers={spanishServers} setServers={setSpanishServers} />
+                        <ServerInputContainer servers={englishServers} setServers={setEnglishServers} />
                     </>
             }
         </div>
