fa594f7c45e10ccad302e04157040fd79a4e08dd
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2024-01-11 09:31:55 -0500

    updated to work with validation

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,45 +4,40 @@ import GenericErrorText from "../components/GenericErrorText";
 import GenericInput from "../components/GenericInput";
 
 export default function SignUp() {
-    const [passwordsMatch, setPasswordsMatch] = useState(true)
-    const [passwordTooShort, setPasswordTooShort] = useState(false)
     const [userExists, setUserExists] = useState(false)
-    const [missingInput, setMissingInput] = useState(false)
+    const [error, setError] = useState(false)
     const navigate = useNavigate()
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         const [username, password, confirmPassword] = [e.target[0].value, e.target[1].value, e.target[2].value]
-        console.log(username, password.length, confirmPassword.length)
 
-        if (!username || !password || !confirmPassword) return setMissingInput(true)
-        setMissingInput(false)
-        if (await doesUserExist(username)) return setUserExists(true);
-        setUserExists(false)
-        if (password !== confirmPassword) return setPasswordsMatch(false);
-        setPasswordsMatch(true)
-        if (password.length < 8 || confirmPassword.length < 8) return setPasswordTooShort(true);
-        setPasswordTooShort(false)
+        setUserExists(await doesUserExist(username))
+        const res = await signUp(username, password, confirmPassword);
+        const data = await res.json()
 
-        signUp(username, password);
+        if (data.error) return setError(data.error);
+        else setError(false)
         navigate('/')
     }
 
     const doesUserExist = async (user) => {
+        user = user || 'unused'
         const res = await fetch(`${import.meta.env.VITE_DEV_URL}/getUser/${user}`);
         const json = await res.json();
         return json.userExists;
     }
 
-    const signUp = (user, pass) => {
-        fetch(`${import.meta.env.VITE_DEV_URL}/signUp`, {
+    const signUp = (user, pass, cnfrmPass) => {
+        return fetch(`${import.meta.env.VITE_DEV_URL}/signUp`, {
             method: "POST",
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
             },
             body: JSON.stringify({
                 username: user,
-                password: pass
+                password: pass,
+                confirmPassword: cnfrmPass
             })
         })
     }
@@ -53,15 +48,8 @@ export default function SignUp() {
                 <GenericInput placeholder="Username" />
                 <GenericInput type="password" placeholder="Password" />
                 <GenericInput type="password" placeholder="Confirm Password" />
-                {missingInput && <GenericErrorText msg="Please fill in all fields!" />}
-                {userExists && <GenericErrorText msg="That username is already taken!" />}
-                {
-                    !passwordsMatch &&
-                    <GenericErrorText msg="Your passwords do not match!" />
-                    ||
-                    passwordTooShort &&
-                    <GenericErrorText msg="Your password must be atleast 8 characters!" />
-                }
+                {error && <GenericErrorText msg={error} />}
+                {userExists && <GenericErrorText msg="Username already exists!" />}
                 <button className="bg-red-500 rounded px-1 mb-10" type="submit">Create Account</button>
                 Already have an account? <Link className="text-blue-700" to={"/login"}>Login here!</Link>
             </form>
