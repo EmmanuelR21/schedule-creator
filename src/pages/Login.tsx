f911ab1959b52f3c21f5d6a1c94a27383d307e79
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2024-01-27 14:57:28 +0000

    removed functionality for revamp

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,37 +1,15 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import GenericErrorText from "../components/genericComponents/GenericErrorText";
+import React from "react";
+import { Link } from "react-router-dom";
 import GenericInput from "../components/genericComponents/GenericInput";
 
 const Login: React.FC = (): JSX.Element => {
-    const [error, setError] = useState<boolean>(false)
-    const navigate = useNavigate()
 
-    const handleLogin = async (e: any): Promise<void> => {
-        e.preventDefault()
-        const [username, password]: [string, string] = [e.target[0].value, e.target[1].value]
-
-        const res: Response = await login(username, password)
-        const data = await res.json()
-
-        if (data.error) return setError(data.error)
-        setError(false)
-
-        if (data.userExists) navigate("/")
-    }
-
-    const login = (user: string, pass: string): Promise<Response> => {
-        user = user || "unused"
-        pass = pass || "0i0"
-        return fetch(`${import.meta.env.VITE_DEV_URL}/login/${user}/${pass}`)
-    }
 
     return (
         <div className="w-full text-xl">
-            <form onSubmit={handleLogin} className="flex m-auto w-96 h-96 flex-col justify-center items-center rounded-lg">
+            <form onSubmit={() => { }} className="flex m-auto w-96 h-96 flex-col justify-center items-center rounded-lg">
                 <GenericInput type="text" placeholder="Username" />
                 <GenericInput type="password" placeholder="Password" />
-                {error && <GenericErrorText msg={`${error}`} />}
                 <button type="submit" className="bg-red-500 rounded px-1 mb-10">Login</button>
                 Don't have an account? <Link className="text-blue-700" to={"/signup"}>Sign-up!</Link>
             </form>
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,55 +1,15 @@
-import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
-import GenericErrorText from "../components/genericComponents/GenericErrorText";
+import { Link } from "react-router-dom";
 import GenericInput from "../components/genericComponents/GenericInput";
 
 const SignUp: React.FC = (): JSX.Element => {
-    const [userExists, setUserExists] = useState<boolean>(false)
-    const [error, setError] = useState<boolean>(false)
-    const navigate = useNavigate()
 
-    const handleSubmit = async (e: any): Promise<void> => {
-        e.preventDefault();
-        const [username, password, confirmPassword]: [string, string, string] = [e.target[0].value, e.target[1].value, e.target[2].value];
-
-        setUserExists(await doesUserExist(username));
-        const res: Response = await signUp(username, password, confirmPassword);
-        const data = await res.json();
-
-        if (data.error) return setError(data.error);
-        else setError(false);
-        navigate('/');
-    }
-
-    const doesUserExist = async (user: string): Promise<any> => {
-        user = user || 'unused'
-        const res = await fetch(`${import.meta.env.VITE_DEV_URL}/getUser/${user}`);
-        const json = await res.json();
-        return json.userExists;
-    }
-
-    const signUp = (user: string, pass: string, cnfrmPass: string): Promise<Response> => {
-        return fetch(`${import.meta.env.VITE_DEV_URL}/signUp`, {
-            method: "POST",
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-            body: JSON.stringify({
-                username: user,
-                password: pass,
-                confirmPassword: cnfrmPass
-            })
-        })
-    }
 
     return (
         <div className="w-full text-xl">
-            <form className="flex m-auto w-96 h-96 flex-col justify-center items-center rounded-lg" onSubmit={handleSubmit}>
+            <form className="flex m-auto w-96 h-96 flex-col justify-center items-center rounded-lg" onSubmit={() => { }}>
                 <GenericInput placeholder="Username" />
                 <GenericInput type="password" placeholder="Password" />
                 <GenericInput type="password" placeholder="Confirm Password" />
-                {error && <GenericErrorText msg={`${error}`} />}
-                {userExists && <GenericErrorText msg="Username already exists!" />}
                 <button className="bg-red-500 rounded px-1 mb-10" type="submit">Create Account</button>
                 Already have an account? <Link className="text-blue-700" to={"/login"}>Login here!</Link>
             </form>
