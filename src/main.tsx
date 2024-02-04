63ab8bc1cfe583aad75aca0e4aeb232796716dae
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2024-02-04 02:49:03 +0000

    fixed some errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.tsx'
 import "./index.css"
-import ContextProvider from './Context/ContextProvider.jsx'
+import ContextProvider from './Context/ContextProvider.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ContextProvider>
